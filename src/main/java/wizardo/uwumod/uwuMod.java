a66e05edd3146f5e87cd552c54fa86765493692c
Wizard-o <Wizardofdoom2@gmail.com>
2020-02-26 19:12:45 -0700

    apparently line 19 of itemInit is causing issues but idk what's wrong

--- src/main/java/wizardo/uwumod/uwuMod.java
+++ src/main/java/wizardo/uwumod/uwuMod.java
@@ -1,26 +1,33 @@
 package wizardo.uwumod;
 
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import wizardo.uwumod.init.itemInit;
 
-@Mod("wizuwumod")
-
+@Mod("wiz_uwumod")
 public class uwuMod {
-
-    public static final String MOD_ID = "uwumod";
-
+    public static final String MOD_ID = "wiz_uwumod";
     public static uwuMod instance;
 
     public uwuMod() {
         instance = this;
 
-        final ModLoadingContext modLoadingContext = ModLoadingContext.get();
-        final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
 
-        itemInit.ITEMS.register(modEventBus);
+        MinecraftForge.EVENT_BUS.register(this);
     }
+    private void setup(final FMLCommonSetupEvent event) {
 
+    }
+    private void doClientStuff(final FMLCommonSetupEvent event) {
+
+    }
+    @SubscribeEvent
+    public void onServerStarting(FMLServerStartingEvent event) {
+
+    }
 }
